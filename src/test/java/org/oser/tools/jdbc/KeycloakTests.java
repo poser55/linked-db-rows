6432d8c0bf3b71d3a1e1fa1cefd7b02dd9c8b9c5
Philipp Oser <Philipp.Oser@elca.ch>
2021-05-29 12:16:26 +0200

    fix error in keycloak test

--- src/test/java/org/oser/tools/jdbc/KeycloakTests.java
+++ src/test/java/org/oser/tools/jdbc/KeycloakTests.java
@@ -15,6 +15,7 @@ class KeycloakTests {
     // docker.exe run -d --name postgres -e POSTGRES_DB=marcopolo-dev -e POSTGRES_USER=marcopolo-dev -e POSTGRES_PASSWORD=password -p 5431:5432 postgres
     // docker.exe exec -it postgres psql -U marcopolo-dev -d marcopolo-dev -c "CREATE SCHEMA IF NOT EXISTS keycloak"
     // docker.exe run --name keycloak -e DB_ADDR=host.docker.internal -e DB_VENDOR=POSTGRES -e DB_DATABASE=marcopolo-dev -e DB_USER=marcopolo-dev -e KEYCLOAK_USER=admin -e DB_PORT=5431 -e KEYCLOAK_PASSWORD=admin -p 8080:8080 jboss/keycloak
+    // go to http://localhost:8080/ to go to the console of keycloak
 
     @Test
     @Disabled // needs keycloak setup
@@ -22,7 +23,8 @@ class KeycloakTests {
         Class.forName("org.postgresql.Driver", true, Thread.currentThread().getContextClassLoader());
 
         DriverDataSource ds = new DriverDataSource(Thread.currentThread().getContextClassLoader(),
-                "org.postgresql.Driver", "jdbc:postgresql://host.docker.internal:5431/marcopolo-dev?currentSchema=keycloak", "marcopolo-dev", "password");
+                "org.postgresql.Driver", "jdbc:postgresql://host.docker.internal:5431/marcopolo-dev?currentSchema=public",
+                "marcopolo-dev", "password");
         Connection con = ds.getConnection();
 
         assertNotNull(con);
@@ -33,7 +35,7 @@ class KeycloakTests {
         // step 2 (remove too much details)
         dbExporter.getStopTablesIncluded().add("keycloak_role");
         // todo: probably you need to adapt UUID to your environment
-        Record exportedCase = dbExporter.contentAsTree(con, "user_entity", "ea0e2ebc-ff0b-4ce4-863f-be70222a7084");
+        Record exportedCase = dbExporter.contentAsTree(con, "user_entity", "98831e8d-898a-4b6c-bc5c-e10ac3911380");
 
         Set<RowLink> allNodes = exportedCase.getAllNodes();
         System.out.println("exportedPerson.size():"+ allNodes.size()+"\n"+ allNodes+"\n\n\n");
